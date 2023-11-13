754ca803901755133ec00ae40aacf8c6aba9ce5b
Mateus Poletto <pole@pop-os.localdomain>
2023-11-13 19:28:42 -0300

    Terminr geração dos relatórios

--- src/main/java/app/shop/aplicacao/relatorios/ServicoRelatorio.java
+++ src/main/java/app/shop/aplicacao/relatorios/ServicoRelatorio.java
@@ -56,6 +56,8 @@ public class ServicoRelatorio {
                 }
             }
         }
-        
+        // Continuar daqui deixando na lista anterior somente os 10 mais vendidos
+        List<RelatorioProdutoDto> relatorio = new ArrayList<>();
+        return relatorio;
     }
 }
